59cb39f12b8b5dbab0e4a15effaa47843bc14124
Ramiro2805 <rlohrmann@udesa.edu.ar>
2023-08-30 13:46:39 -0300

    controller

--- MiProyecto/controller/Controller.js
+++ MiProyecto/controller/Controller.js
@@ -2,19 +2,20 @@ const data = require('../data/modulo-datos')
 
 const controller = {
     register : function(req,res){
-        res.render('registracion',{usuario:data})
+        res.render('registracion',{usuario:data.usuario})
     },
     login : function(req,res){
-        res.render('login',{usuario:data})
+        res.render('login',{usuario:data.usuario})
     },
     miPerfil: function(req,res){
-        res.render('registracion',{usuario:data})
+        res.render('miPerfil',{usuario:data})
     },
     editar : function(req,res){
-        res.render('registracion',{usuario:data})
+        res.render('editarPerfil',{usuario:data.usuario})
     },
     agregar : function(req,res){
-        res.render('registracion',{usuario:data})
+        res.render('agregarPost',{usuario:data})
     }
 
 }
+module.exports = controller
--- MiProyecto/routes/RedSocial.js
+++ MiProyecto/routes/RedSocial.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const data = require("../data/modulo-datos");
-const bandasController = require("../controller/Controller")
+const Controller = require("../controller/Controller");
 
-router.get("/register",)
-router.get('/login',)
-router.get('/miPerfil',)
-router.get('/editar',)
-router.get('/agregar',)
+
+router.get("/register",Controller.register)
+router.get('/login',Controller.login)
+router.get('/miPerfil',Controller.miPerfil)
+router.get('/editar',Controller.editar)
+router.get('/agregar',Controller.agregar)
 
 module.exports = router
