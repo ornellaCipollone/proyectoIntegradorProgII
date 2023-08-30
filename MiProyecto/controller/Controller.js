d6547bba96e6d6f54d887b3ad3e48f08b483823f
Ramiro2805 <rlohrmann@udesa.edu.ar>
2023-08-30 12:51:21 -0300

    controller

--- MiProyecto/controller/Controller.js
+++ MiProyecto/controller/Controller.js
@@ -3,6 +3,18 @@ const data = require('../data/modulo-datos')
 const controller = {
     register : function(req,res){
         res.render('registracion',{usuario:data})
+    },
+    login : function(req,res){
+        res.render('login',{usuario:data})
+    },
+    miPerfil: function(req,res){
+        res.render('registracion',{usuario:data})
+    },
+    editar : function(req,res){
+        res.render('registracion',{usuario:data})
+    },
+    agregar : function(req,res){
+        res.render('registracion',{usuario:data})
     }
 
 }
