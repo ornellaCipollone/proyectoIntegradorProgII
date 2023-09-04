cb24d767b58f2803b1b7538f4d9115046d50a6ed
Ramiro2805 <rlohrmann@udesa.edu.ar>
2023-09-04 18:36:06 -0300

    base de datos

--- MiProyecto/db/data.sql
+++ MiProyecto/db/data.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE IF NOT EXISTS catalogo_producto;
+ CREATE SCHEMA tpProgra;
 
-USE catalogo_producto;
+USE tpProgra;
+
+
 
 CREATE TABLE usuario (
 	id_usuario	INT 	UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -15,3 +17,31 @@ CREATE TABLE usuario (
     update_at 	TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     delete_at 	TIMESTAMP 		NULL
 );
+
+CREATE TABLE posteo (
+	id_posteo	INT 	UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nom         VARCHAR(50) NOT NULL,
+    id_usuario 	INT 	UNSIGNED NOT NULL,
+	FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    created_at 	TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP,
+    update_at 	TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    delete_at 	TIMESTAMP 		NULL
+);
+CREATE TABLE comentarios (
+	id_comentario 	INT 	UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id_usuario 	INT UNSIGNED NOT NULL,
+    id_post 	INT UNSIGNED NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_post) REFERENCES posteo(id_posteo),
+    texto 		VARCHAR(200) 	NOT NULL,
+	created_at 	TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP,
+    update_at 	TIMESTAMP 		DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    delete_at 	TIMESTAMP 		NULL
+    
+);
+INSERT INTO usuario 
+VALUES (DEFAULT,"ramiro","lohrmann","lohrmannramiro@gmail.com","Ramiro",'28/05/2003',44997949,"foto",DEFAULT,DEFAULT,DEFAULT);
+INSERT INTO usuario 
+VALUES (DEFAULT,"Ornella","Cipollone","ornella@gmail.com","Ornella",'01-08-2003',44997950,"foto",DEFAULT,DEFAULT,DEFAULT);
+
+SELECT * FROM usuario;
